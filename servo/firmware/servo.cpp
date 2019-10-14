2064874d6557c5890801ac615995eaf9762ab961
C-pho <fredrik.lagerstedt@gmail.com>
2019-10-14 17:16:23 +0200

    modify servo to fire gun

--- servo/firmware/servo.cpp
+++ servo/firmware/servo.cpp
@@ -2,6 +2,7 @@
 #include <ros.h>
 #include <std_msgs/UInt16MultiArray.h>
 #include <std_msgs/UInt16.h>
+#include <std_msgs/Bool.h>
 
 #include <Arduino.h>
 
@@ -18,6 +19,8 @@ Servo body;
 int pos_init[] = {50, 50, 90, 90, 90};
 
 int servo_pins[] = {9, 10, 11, 12, 13};
+
+
 
 //ROS-setup
 void servo_neck_ex(const std_msgs::UInt16MultiArray& cmd_msg) {
@@ -42,16 +45,30 @@ void servo_body_ex(const std_msgs::UInt16& cmd_msg) {
 	body.write(cmd_msg.data);
 }
 
+void fire_gun_ex(const std_msgs::Bool& cmd_msg) {
+//Check if gun should be fired
+	if (cmd_msg.data) {
+		digitalWrite(8,HIGH);
+	} else {
+		digitalWrite(8,LOW);
+	}
+}
+
 ros::Subscriber<std_msgs::UInt16MultiArray> sub_neck("servo_neck", servo_neck_ex);
 ros::Subscriber<std_msgs::UInt16MultiArray> sub_arm("servo_arm", servo_arm_ex);
 ros::Subscriber<std_msgs::UInt16> sub_body("servo_body", servo_body_ex);
+ros::Subscriber<std_msgs::Bool> fire_gun("fire_gun", fire_gun_ex);
 
 void setup() {
 	hubert.initNode();
 	hubert.subscribe(sub_neck);
 	hubert.subscribe(sub_arm);
-	hubert.subscribe(sub_body);	
+	hubert.subscribe(sub_body);
+	hubert.subscribe(fire_gun);	
 	
+	//Attach fire pin
+	pinMode(8, OUTPUT);
+
 	//Attach each joint servo
 	//and write each init position
 	shoulder.attach(servo_pins[0]);
@@ -69,6 +86,8 @@ void setup() {
 	body.attach(servo_pins[4]);
 	body.write(pos_init[4]);
 	
+	digitalWrite(8, LOW);
+
 	delay(250);
 }
 
